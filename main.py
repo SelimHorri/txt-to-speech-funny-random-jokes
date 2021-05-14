a5c3c221665ee4d1115a3b4ebe19a3febb012558
SelimHorri <horri.slim@gmail.com>
2021-05-14 18:52:10 +0100

    add speak for speech

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 from urllib import request
 import json
 from Joke import Joke
+import pyttsx3
 
 url = "http://official-joke-api.appspot.com/jokes/ten"
 req = request.urlopen(url)
@@ -21,6 +22,8 @@ for j in json_data:
   joke = Joke(setup, punchline)
   jokes.append(joke)
   print(joke)
-
-
+  pyttsx3.speak("setup")
+  pyttsx3.speak(joke.setup)
+  pyttsx3.speak("punchline")
+  pyttsx3.speak(joke.punchline)
 
