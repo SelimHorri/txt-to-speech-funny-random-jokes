b1c27f947c2177e2e0df84531f92512904360c21
SelimHorri <horri.slim@gmail.com>
2021-05-14 18:12:21 +0100

    print punchlines from API

---
+++ Joke.py
@@ -0,0 +1,14 @@
+
+class Joke:
+  
+  def __init__(self, setup, punchline):
+    self.setup = setup
+    self.punchline = punchline
+  
+  def __str__(self):
+    return f"Joke [setup = {self.setup}, punchline = {self.punchline}]"
+  
+  
+  
+
+
--- main.py
+++ main.py
@@ -1,10 +1,26 @@
 
 from urllib import request
+import json
+from Joke import Joke
 
 url = "http://official-joke-api.appspot.com/jokes/ten"
 req = request.urlopen(url)
 
 print(req.getcode())
-print(req.read())
+
+data = req.read()
+json_data = json.loads(data)
+
+#print(json_data)
+
+jokes = []
+
+for j in json_data:
+  setup = j["setup"]
+  punchline = j["punchline"]
+  joke = Joke(setup, punchline)
+  jokes.append(joke)
+  print(joke)
+
 
 
